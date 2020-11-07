20b434fd1ea7849cf29df420b6bd55cd78ea88cc
Zhou Jian <zhoujian0998@gmail.com>
2020-11-07 12:12:12 +0100

    Add mocha eslint

---
+++ mocha.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: "./index.js",
+  rules: {
+    "object-curly-newline": [
+      "error", {
+        ObjectExpression: {
+          multiline: true,
+          minProperties: 1,
+        },
+      },
+    ],
+    "max-nested-callbacks": [
+      "error", {
+        max: 3,
+      },
+    ],
+    "padded-blocks": [
+      "error", "always", {
+        allowSingleLineBlocks: true,
+      },
+    ],
+    "function-paren-newline": ["error", "multiline"],
+    "func-names": ["off"],
+    "prefer-arrow-callback": ["off"],
+    "no-magic-numbers": ["off"],
+    "no-invalid-this": ["off"],
+  },
+}
